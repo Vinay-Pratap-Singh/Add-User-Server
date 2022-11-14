8a6e8f01a2f41e7d87d9a5325268b00c3553ef91
Vinay Pratap Singh <technicalharvi@gmail.com>
2022-11-14 18:26:48 +0530

    added urlencoded

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 // creating our app using express
@@ -10,47 +10,52 @@ const port = process.env.PORT;
 
 // connecting the database
 try {
-    mongoose.connect(db);
-    console.log("DB connected succesfully");
+  mongoose.connect(db);
+  console.log("DB connected succesfully");
 } catch (error) {
-    console.log("DB connection fails\n",error);
+  console.log("DB connection fails\n", error);
 }
+
+// using express.json to convert our data in json format
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // creating the user schema
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required:true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+});
 
 // creating a user model
 const user = mongoose.model("user", userSchema);
 
 // simple get request to display the message
 app.get("/", (req, res) => {
-    res.send("Vinay");
-})
-
-// using express.json to convert our data in json format
-app.use(express.json());
+  res.send("Vinay");
+});
 
 // getting the data using post request
 app.post("/", (req, res) => {
-    // accessing the user data by req.body
-    console.log(req.body.name);
-    // if there are multiple data, then i can destructure it
-    const name = req.body.name;
-    // creating the new document with name data, data should be in object and same as userschema 
-    const newUser = new user({name});
-    // saving our data in the database
-    newUser.save().then((result) => {
-        console.log("user added sucessfully");
-    }).catch((err) => {
-        console.log("user adding failed\n", err);
+  // accessing the user data by req.body
+  // console.log(req.body.name);
+  console.log(req.body);
+  // if there are multiple data, then i can destructure it
+  const name = req.body.name;
+  // creating the new document with name data, data should be in object and same as userschema
+  const newUser = new user({ name });
+  // saving our data in the database
+  newUser
+    .save()
+    .then((result) => {
+      console.log("user added sucessfully");
+    })
+    .catch((err) => {
+      console.log("user adding failed\n", err);
     });
-})
+});
 
 app.listen(port, () => {
-    console.log("Server is running");
-})
+  console.log(`Server is running at ${port}`);
+});
